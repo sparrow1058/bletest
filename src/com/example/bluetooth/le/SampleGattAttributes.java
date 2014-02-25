9ae82dff6bcada126ca9fdaecf345c204529b6d2
unknown <cc@COJRQVNVMZHYOXJ.(none)>
2014-02-25 12:50:20 +0800

    First Version

---
+++ src/com/example/bluetooth/le/SampleGattAttributes.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.bluetooth.le;
+
+import java.util.HashMap;
+
+/**
+ * This class includes a small subset of standard GATT attributes for demonstration purposes.
+ */
+public class SampleGattAttributes {
+    private static HashMap<String, String> attributes = new HashMap();
+    public static String HEART_RATE_MEASUREMENT = "0000C004-0000-1000-8000-00805f9b34fb";
+    public static String CLIENT_CHARACTERISTIC_CONFIG = "00002902-0000-1000-8000-00805f9b34fb";
+
+    static {
+        // Sample Services.
+        attributes.put("0000fff00000-1000-8000-00805f9b34fb", "Heart Rate Service");
+        attributes.put("0000180a-0000-1000-8000-00805f9b34fb", "Device Information Service");
+        attributes.put("00001802-0000-1000-8000-00805f9b34fb", "IMMEDIATE Alert Service");		
+        attributes.put("00001803-0000-1000-8000-00805f9b34fb", "Link Loss Service");
+        attributes.put("00001804-0000-1000-8000-00805f9b34fb", "TX Power Level Service");
+        attributes.put("0000180F-0000-1000-8000-00805f9b34fb", "Battery Service");
+        // Sample Characteristics.
+        attributes.put(HEART_RATE_MEASUREMENT, "Heart Rate Measurement");
+        attributes.put("00002a29-0000-1000-8000-00805f9b34fb", "Manufacturer Name String");
+        attributes.put("00002a06-0000-1000-8000-00805f9b34fb", "Alert Level");
+        attributes.put("00002a07-0000-1000-8000-00805f9b34fb", "TX PWR Level");
+        attributes.put("00002a19-0000-1000-8000-00805f9b34fb", "Battery param");
+        
+    }
+    
+    public static String lookup(String uuid, String defaultName) {
+        String name = attributes.get(uuid);
+        return name == null ? defaultName : name;
+    }
+}
+
+ 
